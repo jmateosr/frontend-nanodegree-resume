cd77cfa477395c136812a8cccda7679e754fc469
jmateosr <xabylecter@gmail.com>
2014-12-24 12:18:59 +0100

    Modifications done while studying Lesson 1

---
+++ js/resumeBuilder.js
@@ -0,0 +1,24 @@
+var awesomeThoughts = "I am Javi and I am AWESOME";
+
+var funThoughts = awesomeThoughts.replace("AWESOME","FUN");
+
+console.log(awesomeThoughts);
+
+console.log(funThoughts);
+
+var name = "Javier Mateos";
+
+var formattedName = HTMLheaderName.replace("%data%",name);
+
+var role = "Online Marketing Specialist"
+
+var formattedRole = HTMLheaderRole.replace("%data%",role);
+
+$("#header").prepend(formattedRole);
+
+$("#header").prepend(formattedName);
+
+var string1 = "audacity";
+var string2 = string1.slice(1);
+
+$("#header").prepend(string2);
